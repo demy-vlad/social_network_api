fea97d6bbd088ff34fb9940eabb0bf3973cee209
Vlad <you@example.com>
2024-08-09 19:00:30 +0300

    mb fix

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ from app.views.chats import chat_router
 from app.views.messages import message_router
 from app.database import engine, Base
 import json
+from .database import SessionLocal
+from app.models import Message
+from sqlalchemy.orm import Session
+from datetime import datetime
 
 app = FastAPI()
 
@@ -22,20 +26,50 @@ logging.basicConfig(level=logging.DEBUG)
 # app.include_router(chat_router)
 app.include_router(message_router)
 
+
+connections = []  # This should be a list to keep track of all connected clients
+
 @app.websocket("/ws/chat/{chat_id}/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, chat_id: str, user_id: str):
+async def websocket_endpoint(websocket: WebSocket, user_id: str, chat_id: str):
     await websocket.accept()
-    logging.info(f"User {user_id} connected to chat {chat_id}")
+    connections.append(websocket)
+    db = SessionLocal()  # Create or get your database session here
+    
     try:
         while True:
             data = await websocket.receive_text()
-            logging.info(f"Received message: {data} from User ID: {user_id}")
-            # Отправка сообщения всем клиентам
-            await websocket.send_text(f"Message from {user_id}: {data}")
-    except WebSocketDisconnect as e:
-        logging.error(f"WebSocket disconnected: {e}")
-    except Exception as e:
-        logging.error(f"Unexpected error: {e}")
+            print(f"Received raw message: {data} from User ID: {user_id}")
+
+            try:
+                # Attempt to parse the JSON data
+                message_data = json.loads(data)
+                print(f"Parsed message data: {message_data}")
+
+                # Save the message to the database
+                save_message_to_db(message_data['message'], message_data['userId'], db, chat_id)
+
+                # Broadcast the message to all connected clients
+                for connection in connections:
+                    if connection != websocket:
+                        await connection.send_text(data)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+    except WebSocketDisconnect:
+        print(f"User {user_id} disconnected")
+    finally:
+        connections.remove(websocket)
+        db.close()
+
+def save_message_to_db(message: str, user_id: str, db: Session, chat_id: str):
+    db_message = Message(
+        message=message,
+        timestamp=datetime.utcnow(),
+        user_id=user_id,
+        chat_id=chat_id
+    )
+    db.add(db_message)
+    db.commit()
+    db.refresh(db_message)
 
 # Define a route for the root URL
 @app.get("/", response_class=FileResponse)
